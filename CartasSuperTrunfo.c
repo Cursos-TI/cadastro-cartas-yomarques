2fc008eb6f1c26c0c3d105dbd585e188aeafa074
Marquess <juniormarques2402@gmail.com>
2026-04-20 21:34:21 +0000

    Reajuste da escolha de dois atributos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> 
 
 // Desafio Super Trunfo - Países
-//Desenvolvimento do jogo com inclusão de switch case e if-else. 16/04/2026
+//Desenvolvimento do jogo com inclusão de switch casa, if - else e operadores.
 
 int main() {
     //Definição das variáveis para armazenar os dados das cartas 1 e 2.
     char estado1 [80], estado2 [80];
     char cidade1[40], cidade2[40];
+    char nomeAtributo1[90], nomeAtributo2[90];
 
+    int Atributo1, Atributo2;
     int pontosturisticos1, pontosturisticos2, EscolhaJogador;
     unsigned long int populacao1, populacao2;
 
     float area1, area2, PIB1, PIB2;
     float densidadepopulacional1, densidadepopulacional2;
-    float PIBpercapita1 = PIB1 / populacao1, PIBpercapita2 = PIB2 / populacao2;
-    float SuperPoder1, SuperPoder2;
+    float PIBpercapita1, PIBpercapita2, SuperPoder1, SuperPoder2;
 
+    float Valor1A, Valor2A, Valor1B, Valor2B;
+    float soma1, soma2;
+    
     //Apresentação do jogo e escolha do jogador para iniciar o jogo ou ver as regras.
     printf("Bem vindo ao Super Trunfo!\n\n");
     printf("Escolha uma opção:\n");
@@ -24,22 +29,22 @@ int main() {
     printf("2 - Ver Regras\n");
     scanf("%d", &EscolhaJogador);
 
+    //Estrutura Switch com menu interativo simples;
     switch (EscolhaJogador) {
         case 1:
-            printf("Iniciando o jogo...\n\n");
+        printf("Iniciando o jogo...\n\n");
             break;
         case 2:
-            printf("Regras do Jogo:\n");
-            printf("1. Cada jogador deve cadastrar os dados de um estado e sua respectiva cidade.\n");
-            printf("2. Os atributos cadastrados serão: nome do estado, nome da cidade, população, pontos turísticos, área e PIB.\n");
-            printf("3. O Super Poder de cada carta será calculado com base nos atributos cadastrados.\n");
-            printf("4. O jogador escolherá um atributo para comparar entre as duas cartas, e o vencedor será determinado com base nesse atributo.\n");
-            printf("5. O objetivo é ter a carta com o maior Super Poder ou o melhor desempenho no atributo escolhido.\n\n");
+        printf("Regras do Jogo:\n");
+        printf("1. Cada jogador deve cadastrar os dados de um estado e sua respectiva cidade.\n");
+        printf("2. Os atributos cadastrados serão: nome do estado, nome da cidade, população, pontos turísticos, área e PIB.\n");
+        printf("3. O Super Poder de cada carta será calculado com base nos atributos cadastrados.\n");
+        printf("4. O jogador escolherá um atributo para comparar entre as duas cartas, e o vencedor será determinado com base nesse atributo.\n");
+        printf("5. O objetivo é ter a carta com o maior Super Poder ou o melhor desempenho no atributo escolhido.\n\n");
             break;
         default:
-            printf("Opção inválida! Por favor, escolha 1 para iniciar o jogo ou 2 para ver as regras.\n\n");
+        printf("Opção inválida! Por favor, escolha 1 para iniciar o jogo ou 2 para ver as regras.\n\n");
     }
-
     //Entradada de dados para a carta 1.
     printf("Digite o nome do Estado 1: \n");
     scanf("%s", estado1);
@@ -54,9 +59,11 @@ int main() {
     printf("Digite o PIB do estado 1: \n");
     scanf("%f", &PIB1);
 
- printf("Densidade Populacional %s (Quanto menor o valor, maior o poder!): %f habitantes por quilômetro quadrado!\n\n", 
-                                     estado1, densidadepopulacional1 = populacao1 / area1);
- printf("PIB per capita 1: %f\n\n", PIBpercapita1 = PIB1 / populacao1);
+ //Cálculo para densidade populacional e PIB per capita e exibição do resultado.  
+ densidadepopulacional1 = populacao1 / area1;
+ printf("Densidade Populacional %s (Quanto menor o valor, maior o poder!): %.2f habitantes por Km²!\n", estado1, densidadepopulacional1);
+ PIBpercapita1 = PIB1 / populacao1;
+ printf("PIB per capita 1: %f\n\n", PIBpercapita1);
 
     //Entrada de dados para a carta 2.
     printf("Digite o nome do Estado 2:\n");
@@ -72,157 +79,152 @@ int main() {
     printf("Digite o PIB do estado 2:\n");
     scanf("%f", &PIB2);
 
- printf("Densidade Populacional %s (Quanto menor o valor, maior o poder!): %f habitantes por quilômetro quadrado!\n\n", 
-                                     estado2, densidadepopulacional2 = populacao2 / area2);
- printf("PIB per capita 2: %f\n\n", PIBpercapita2 = PIB2 / populacao2);
+ //Cálculo para densidade populacional e PIB per capita e exibição do resultado.
+ densidadepopulacional2 = populacao2 / area2;
+ printf("Densidade Populacional %s (Quanto menor o valor, maior o poder!): %.2f habitantes por Km²!\n", estado2, densidadepopulacional2);
+ PIBpercapita2 = PIB2 / populacao2;
+ printf("PIB per capita 2: %f\n\n", PIBpercapita2);
 
-//Impressão dos dados cadastrados, carta 01 e carta 02 e cálculo do Super Poder de cada carta.
+ //Impressão dos dados cadastrados, carta 01 e carta 02 e cálculo do Super Poder de cada carta.
  printf("Estado 1: %s - Cidade 1: %s\n", estado1, cidade1);
  printf("População 1: %lu Habitantes - Pontos Turísticos 1: %d\n", populacao1, pontosturisticos1);
  printf("Área 1 (km²): %.2f\n", area1);
  printf("PIB 1: %.2f\n", PIB1);
 
- SuperPoder1 = pontosturisticos1 + densidadepopulacional1 + PIB1 + PIBpercapita1 + (area1 / populacao1);
- printf("Super Poder do Estado 1 (%s): %.2f\n", estado1, SuperPoder1);
+ //Cálculo do Super poder e a regra de densidade populacional;
+ SuperPoder1 = pontosturisticos1 + densidadepopulacional1 + PIB1 + PIBpercapita1 + area1 + populacao1;
+ printf("Super Poder do Estado (%s): %.2f\n\n", estado1, SuperPoder1);
 
  printf("Estado 2: %s - Cidade 2: %s\n", estado2, cidade2);
  printf("População 2: %lu Habitantes - Pontos Turísticos 2: %d\n", populacao2, pontosturisticos2);
  printf("Área 2 (km²): %.2f\n", area2);
  printf("PIB 2: %.2f\n", PIB2);
 
- SuperPoder2 = pontosturisticos2 + densidadepopulacional2 + PIB2 + PIBpercapita2 + (area2 / populacao2);
- printf("Super Poder do Estado 2 (%s): %.2f\n", estado2, SuperPoder2);
+ //Cálculo do Super poder e a regra de densidade populacional;
+ SuperPoder2 = pontosturisticos2 + densidadepopulacional2 + PIB2 + PIBpercapita2 + area2 + populacao2;
+ printf("Super Poder do Estado (%s): %.2f\n", estado2, SuperPoder2);
 
-//Comparação para determinar o vencedor.
-printf("\n!!Comparação dos Super Poderes e Atributos!!\n");
-printf("Lembre-se, Valor 1 = Verdadeiro, Valor 0 = Falso\n\n");
+ //Comparação das cartas 01 e 02;
+ //Menu interativo para que o jogador escolha o atributo desejado.
 
-printf("O Super Poder do Estado 1 (%s) é maior que o do Estado (%s)? %d\n\n", estado1, estado2, SuperPoder1 > SuperPoder2);
-printf("O estado (%s) tem mais pontos turísticos que o estado (%s)? %d\n\n", estado1, estado2,
-                                                          pontosturisticos1 > pontosturisticos2);
+  printf("\nAgora você deve escolher dois atributos para comparar e determinar o vencedor final!\n\n");
+  printf("Escolha o atributo 01:\n");
+  printf("1 - Super Poder\n");
+  printf("2 - Pontos Turísticos\n");
+  printf("3 - Densidade Populacional\n");
+  printf("4 - PIB\n");
+  printf("5 - PIB per capita\n");
+  printf("6 - Área\n");
+  printf("7 - População\n");
+  scanf("%d", &Atributo1);
 
-printf("O estado (%s) tem uma densidade populacional menor que o estado (%s)? %d\n\n", estado1, estado2,
-                                                          densidadepopulacional1 < densidadepopulacional2);  
+  printf("Escolha o atributo 02:\n");
+   printf("1 - Super Poder\n");
+   printf("2 - Pontos Turísticos\n");
+   printf("3 - Densidade Populacional\n");
+   printf("4 - PIB\n");
+   printf("5 - PIB per capita\n");
+   printf("6 - Área\n");
+   printf("7 - População\n");
+  scanf("%d", &Atributo2);
 
-printf("O estado (%s) tem um PIB maior que o estado (%s)? %d\n\n", estado1, estado2, PIB1 > PIB2);
-printf("O estado (%s) tem um PIB per capita maior que o estado (%s)? %d\n\n", estado1, estado2, PIBpercapita1 > PIBpercapita2);
-printf("O estado (%s) tem uma área maior que o estado (%s)? %d\n\n", estado1, estado2, area1 > area2);
-printf("O estado (%s) tem um número maior de habitantes que o estado (%s)? %d\n\n", estado1, estado2, populacao1 > populacao2);
+  //Controle de  escolha para evitar que o usuário escolha o mesmo atributo
+  if (Atributo2 == Atributo1) {
+    printf ("Você escolheu o mesmo atributo! Por favor, tente novamente!");
+  }
+  // >>> ATRIBUTO 1 <<<
+   switch (Atributo1) {
+        case 1:
+         Valor1A = SuperPoder1; Valor2A = SuperPoder2;
+         printf("Atributo 1: %s\n", "Super Poder");
+         break;
+        case 2:
+         Valor1A = pontosturisticos1; Valor2A = pontosturisticos2;
+         printf("Atributo 1: %s\n", "Pontos Turisticos");
+         break;
+        case 3:
+         Valor1A = densidadepopulacional1; Valor2A = densidadepopulacional2;
+         printf("Atributo 1: %s\n", "Densidade");
+         break;
+        case 4:
+         Valor1A = PIB1; Valor2A = PIB2;
+         printf("Atributo 1: %s\n", "PIB");
+         break;
+        case 5:
+         Valor1A = PIBpercapita1; Valor2A = PIBpercapita2;
+         printf("Atributo 1: %s\n", "PIB per capita");
+         break;
+        case 6:
+         Valor1A = area1; Valor2A = area2;
+         printf("Atributo 1: %s\n", "Area");
+         break;
+        case 7:
+         Valor1A = populacao1; Valor2A = populacao2;
+         printf("Atributo 1: %s\n", "Populacao");
+         break;
+   }
+    // >>> ATRIBUTO 2 <<<
+    switch (Atributo2) {
+        case 1:
+         Valor1B = SuperPoder1; Valor2B = SuperPoder2;
+         printf("Atributo 2: %s\n", "Super Poder");
+         break;
+        case 2:
+         Valor1B = pontosturisticos1; Valor2B = pontosturisticos2;
+         printf("Atributo 2: %s\n", "Pontos Turisticos");
+         break;
+        case 3:
+         Valor1B = densidadepopulacional1; Valor2B = densidadepopulacional2;
+         printf("Atributo 2: %s\n", "Densidade");
+         break;
+        case 4:
+         Valor1B = PIB1; Valor2B = PIB2;
+         printf("Atributo 2: %s\n", "PIB");
+         break;
+        case 5:
+         Valor1B = PIBpercapita1; Valor2B = PIBpercapita2;
+         printf("Atributo 2: %s\n", "PIB per capita");
+         break;
+        case 6:
+         Valor1B = area1; Valor2B = area2;
+         printf("Atributo 2: %s\n", "Area");
+         break;
+        case 7:
+         Valor1B = populacao1; Valor2B = populacao2;
+         printf("Atributo 2: %s\n", "Populacao");
+         break;
+    }
+    // >>> AJUSTE DA DENSIDADE <<<
+    // Aqui aplicamos a regra: menor densidade = melhor poder!
+    if (Atributo1 == 3) {
+        Valor1A = 1 / Valor1A;
+        Valor2A = 1 / Valor2A;
+    }
+    if (Atributo2 == 3) {
+        Valor1B = 1 / Valor1B;
+        Valor2B = 1 / Valor2B;
+    }
+    // >>> SOMA <<<
+    soma1 = Valor1A + Valor1B;
+    soma2 = Valor2A + Valor2B;
 
-printf("Escolha um atributo para fazer a comparação final:\n");
-printf("1 - Super Poder\n");
-printf("2 - Pontos Turísticos\n");
-printf("3 - Densidade Populacional\n");
-printf("4 - PIB\n");
-printf("5 - PIB per capita\n");
-printf("6 - Área\n");
-printf("7 - População\n");
-scanf("%d", &EscolhaJogador);
+ // >>> RESULTADO <<<
+ printf("\n Resultado Final do Jogo!!\n\n");
+ printf("%s vs %s\n\n", estado1, estado2);
 
-switch (EscolhaJogador) {
-    case 1:
-        printf("Você escolheu comparar o Super Poder!\n");
-        if (SuperPoder1 > SuperPoder2) {
-            printf("O estado (%s) é o vencedor!\n", estado1);
-            printf("Super Poder do estado (%s): %.2f\n", estado1, SuperPoder1);
-            printf("Super Poder do estado (%s): %.2f\n", estado2, SuperPoder2);
-        } else if (SuperPoder1 < SuperPoder2) {
-            printf("O estado (%s) é o vencedor!\n", estado2);
-            printf("Super Poder do estado (%s): %.2f\n", estado1, SuperPoder1);
-            printf("Super Poder do estado (%s): %.2f\n", estado2, SuperPoder2);
-        } else {
-            printf("Empate! Ambos os estados têm o mesmo Super Poder.\n");
-        }
-        break;
-    case 2:
-        printf("Você escolheu comparar os Pontos Turísticos!\n");
-        if (pontosturisticos1 > pontosturisticos2) {
-            printf("O estado (%s) é o vencedor!\n", estado1);
-            printf("Pontos Turísticos do estado (%s): %d\n", estado1, pontosturisticos1);
-            printf("Pontos Turísticos do estado (%s): %d\n", estado2, pontosturisticos2);
-        } else if (pontosturisticos1 < pontosturisticos2) {
-            printf("O estado (%s) é o vencedor!\n", estado2);
-            printf("Pontos Turísticos do estado (%s): %d\n", estado1, pontosturisticos1);
-            printf("Pontos Turísticos do estado (%s): %d\n", estado2, pontosturisticos2);
-        } else {
-            printf("Empate! Ambos os estados têm a mesma quantidade de pontos turísticos.\n");
-        }
-        break;
-    case 3:
-        printf("Você escolheu comparar a Densidade Populacional!\n");
-        printf("Lembre-se, quanto menor a densidade populacional, maior o poder!\n");
-        if (densidadepopulacional1 < densidadepopulacional2) {
-            printf("O estado (%s) é o vencedor!\n", estado1);
-            printf("Densidade Populacional do estado (%s): %.2f habitantes por km²\n", estado1, densidadepopulacional1);
-            printf("Densidade Populacional do estado (%s): %.2f habitantes por km²\n", estado2, densidadepopulacional2);
-        } else if (densidadepopulacional1 > densidadepopulacional2) {
-            printf("O estado (%s) é o vencedor!\n", estado2);
-            printf("Densidade Populacional do estado (%s): %.2f habitantes por km²\n", estado1, densidadepopulacional1);
-            printf("Densidade Populacional do estado (%s): %.2f habitantes por km²\n", estado2, densidadepopulacional2);
-        } else {
-            printf("Empate! Ambos os estados têm a mesma densidade populacional.\n");
-        }
-        break;
-    case 4:
-        printf("Você escolheu comparar o PIB!\n");
-        if (PIB1 > PIB2) {
-            printf("O estado (%s) é o vencedor!\n", estado1);
-            printf("PIB do estado (%s): %.2f\n", estado1, PIB1);
-            printf("PIB do estado (%s): %.2f\n", estado2, PIB2);
-        } else if (PIB1 < PIB2) {
-            printf("O estado (%s) é o vencedor!\n", estado2);
-            printf("PIB do estado (%s): %.2f\n", estado1, PIB1);
-            printf("PIB do estado (%s): %.2f\n", estado2, PIB2);
-        } else {
-            printf("Empate! Ambos os estados têm o mesmo PIB.\n");
-        }
-        break;
-    case 5:
-        printf("Você escolheu comparar o PIB per capita!\n");
-        if (PIBpercapita1 > PIBpercapita2) {
-            printf("O estado (%s) é o vencedor!\n", estado1);
-            printf("PIB per capita do estado (%s): %.2f\n", estado1, PIBpercapita1);
-            printf("PIB per capita do estado (%s): %.2f\n", estado2, PIBpercapita2);
-        } else if (PIBpercapita1 < PIBpercapita2) {
-            printf("O estado (%s) é o vencedor!\n", estado2);
-            printf("PIB per capita do estado (%s): %.2f\n", estado1, PIBpercapita1);
-            printf("PIB per capita do estado (%s): %.2f\n", estado2, PIBpercapita2);
-        } else {
-            printf("Empate! Ambos os estados têm o mesmo PIB per capita.\n");
-        }
-        break;
-    case 6:
-        printf("Você escolheu comparar a Área!\n");
-        if (area1 > area2) {
-            printf("O estado (%s) é o vencedor!\n", estado1);
-            printf("Área do estado (%s): %.2f km²\n", estado1, area1);
-            printf("Área do estado (%s): %.2f km²\n", estado2, area2);
-        } else if (area1 < area2) {
-            printf("O estado (%s) é o vencedor!\n", estado2);
-            printf("Área do estado (%s): %.2f km²\n", estado1, area1);
-            printf("Área do estado  (%s): %.2f km²\n", estado2, area2);
-        } else {
-            printf("Empate! Ambos os estados têm a mesma área.\n");
-        }
-        break;
-    case 7:
-        printf("Você escolheu comparar a População!\n");
-        if (populacao1 > populacao2) {
-            printf("O estado (%s) é o vencedor!\n", estado1);
-            printf("População do estado (%s): %lu habitantes\n", estado1, populacao1);
-            printf("População do estado (%s): %lu habitantes\n", estado2, populacao2);
-        } else if (populacao1 < populacao2) {
-            printf("O estado (%s) é o vencedor!\n", estado2);
-            printf("População do estado (%s): %lu habitantes\n", estado1, populacao1);
-            printf("População do estado (%s): %lu habitantes\n", estado2, populacao2);
-        } else {
-            printf("Empate! Ambos os estados têm a mesma população.\n");
-        }
-        break;
-    default:
-        printf("Opção Inválida! Por favor, escolha um número entre 1 e 7.\n");
-}
+ printf("%s:\n%s = %.2f | %s = %.2f\n\n", nomeAtributo1, estado1, Valor1A, estado2, Valor2A);
+ printf("%s:\n%s = %.2f | %s = %.2f\n\n", nomeAtributo2, estado1, Valor1B, estado2, Valor2B);
+ printf("Soma %s: %.2f\n", estado1, soma1);
+ printf("Soma %s: %.2f\n\n", estado2, soma2);
 
-return 0;
+    if (soma1 > soma2)
+        printf("Parabéns! O Vencedor é: %s\n", estado1);
+    else if (soma2 > soma1)
+        printf("Parabéns! O Vencedor é: %s\n", estado2);
+    else
+        printf("Empate! Ambos os valores são iguais!\n");
+
+ return 0;
 
 }
+
